4d146aab811974fcd42da48c945cde83fa4dbce0
Tom Anderson <tom@lupin.home>
2014-09-24 09:43:12 -0400

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Tom Anderson on 9/24/14.
+//  Copyright (c) 2014 Maritom LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
